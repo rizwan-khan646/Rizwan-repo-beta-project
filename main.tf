e0464b40ed8c65a0db9ed5df7b2507418d0311b0
rizwan <rizwanwills@gmail.comm>
2024-08-10 16:06:01 +0530

    code chala gaya

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.115.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "lol-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "rg-01" {
+  name     = "loll-rg"
+  location = "West Europe"
+}
